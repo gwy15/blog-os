b02abb0be02c6f84a23b25a4253dbd4a364c9cd2
gwy15 <gwy15thu@gmail.com>
2020-06-24 20:11:23 +0800

    chapter 2. minimal kernel

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,15 @@
 
 #[no_mangle] // no name mangle
 pub extern "C" fn _start() -> ! {
+    const VGA_BUFFER: *mut u8 = 0xb8000 as *mut u8;
+
+    for (i, &byte) in b"Hello, World".iter().enumerate() {
+        unsafe {
+            *VGA_BUFFER.offset(i as isize * 2) = byte;
+            *VGA_BUFFER.offset(i as isize * 2 + 1) = 0xb;
+        }
+    }
+
     loop {}
 }
 
